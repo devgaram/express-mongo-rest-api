b860b215d5c45a7fc38cd41208ad81646ff1ecf9
devgaram <joingaram@gmail.com>
2020-03-28 00:26:00 +0900

    Add localhost to whitelist for cors

--- src/config/express.js
+++ src/config/express.js
@@ -12,11 +12,11 @@ const { env } = config;
 const { errorDebug } = debug;
 
 
-const whitelist = ['http://example1.com', 'http://example2.com'];
+const whitelist = ['http://localhost:3000', 'http://example2.com'];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (origin == null /* || whitelist.indexOf(origin) !== -1 */) {
+    if (whitelist.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(createError('Not allowed by CORS'));
@@ -29,10 +29,10 @@ if (env === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(helmet());
-app.use(cors(corsOptions));
 app.use('/api', api);
 
 // Catch 404 and forward to error handler
